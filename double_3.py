6f639b74de82040e72536ea73c19e2a0fdf23b37
Han-kookhee <161431425+Han-kookhee@users.noreply.github.com>
2024-02-28 16:14:58 +0900

    double_3
    3의 배수의 합계를 구하는 프로그램

---
+++ double_3.py
@@ -0,0 +1,9 @@
+sum=0
+count=0
+for i in range (1,100):
+    if i % 3 ==0:
+        count=count+1
+        sum=sum+i
+print (" 1부터 100까지의 3의 배수의 갯수: %d"  %count)
+print (" 1부터 100까지의 3의 배수의 합계: %d"  %sum)
+            
